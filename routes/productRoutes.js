cbeca15fb286adc96f7dd1b9d71924008f640921
unknown <pngbao20@clc.fitus.edu.vn>
2024-11-18 15:58:51 +0700

    change the repo structure
    đổi lại cấu trúc folder

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('d:/Web - 22KTPM1/GA04/Controllers/productController');
+const productController = require('../Controllers/productController');
 
 router.get('/products', productController.listProducts);
 router.get('/products/:id', productController.productDetail);
